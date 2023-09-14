b177e8db60023360790e3c47dc1edf9ea0d79764
Elmanuff <familiama24@gmail.com>
2023-09-13 22:37:54 -0600

    Se crearon clase y ventana para empezar e juego

--- src/main/java/com/Clientes/VentanaClienteController.java
+++ src/main/java/com/Clientes/VentanaClienteController.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class VentanaClienteController {
+    @FXML
     public Button botonUnirse;
     @FXML
     private TextField textPuerto;
---
+++ src/main/java/com/Clientes/VentanaJuegoController.java
@@ -0,0 +1,4 @@
+package com.Clientes;
+
+public class VentanaJuegoController {
+}
