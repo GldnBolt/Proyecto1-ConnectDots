6e4b0b6d18716ddb7ea92af79fcce2da2b7c917f
Elmanuff <familiama24@gmail.com>
2023-09-19 23:08:15 -0600

    Se solucionaron errores

--- src/main/java/com/Clientes/VentanaJuegoController.java
+++ src/main/java/com/Clientes/VentanaJuegoController.java
@@ -11,38 +11,12 @@ import javafx.stage.Stage;
 
 public class VentanaJuegoController{
     @FXML
-    public Button Boton11;
-    @FXML
-    public Button Boton12;
-    @FXML
-    public Button Boton13;
-    @FXML
-    public Button Boton14;
-    @FXML
-    public Button Boton21;
-    @FXML
-    public Button Boton22;
-    @FXML
-    public Button Boton23;
-    @FXML
-    public Button Boton24;
-    @FXML
-    public Button Boton31;
-    @FXML
-    public Button Boton32;
-    @FXML
-    public Button Boton33;
-    @FXML
-    public Button Boton34;
-    @FXML
-    public Button Boton41;
-    @FXML
-    public Button Boton42;
-    @FXML
-    public Button Boton43;
-    @FXML
-    public Button Boton44;
+    private Button Boton11;
 
-    if
+    public void seleccionado(){
+        //boton.getid
+        Boton11.setStyle("-fx-background-color: green");
     }
+
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,8 +5,8 @@ module com.proyecto1connectdots {
     // requires RXTXcomm;
 
     exports com;
-    opens com to javafx.fxml;
     exports com.Clientes;
+    opens com to javafx.fxml;
     opens com.Clientes to javafx.fxml;
     exports com.Servidor;
     opens com.Servidor to javafx.fxml;
