6d9ba49146cde59e4129be0f9e34acc8a8f7c9e2
Andres Blanco Coto <andblanco@estudiantec.cr>
2023-09-19 22:23:35 -0600

    se pusieron los botones

--- src/main/java/com/Clientes/VentanaJuegoController.java
+++ src/main/java/com/Clientes/VentanaJuegoController.java
@@ -11,7 +11,38 @@ import javafx.stage.Stage;
 
 public class VentanaJuegoController{
     @FXML
-    Button Boton11;
+    public Button Boton11;
+    @FXML
+    public Button Boton12;
+    @FXML
+    public Button Boton13;
+    @FXML
+    public Button Boton14;
+    @FXML
+    public Button Boton21;
+    @FXML
+    public Button Boton22;
+    @FXML
+    public Button Boton23;
+    @FXML
+    public Button Boton24;
+    @FXML
+    public Button Boton31;
+    @FXML
+    public Button Boton32;
+    @FXML
+    public Button Boton33;
+    @FXML
+    public Button Boton34;
+    @FXML
+    public Button Boton41;
+    @FXML
+    public Button Boton42;
+    @FXML
+    public Button Boton43;
+    @FXML
+    public Button Boton44;
 
-
+    if
+    }
 }
