fe2926ac8ee88f16e6d1d04f8ee31858db2a5d32
genecmmm <129640770+genecmmm@users.noreply.github.com>
2023-09-20 10:34:58 -0600

    se pusieron las lineas

--- src/main/java/com/Clientes/VentanaJuegoController.java
+++ src/main/java/com/Clientes/VentanaJuegoController.java
@@ -2,20 +2,20 @@ package com.Clientes;
 
 import javafx.application.Application;
 import java.io.IOException;
-import java.util.EventObject;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
-public class VentanaJuegoController {
-
+public class VentanaJuegoController{
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        Button clickedButton = (Button) event.getSource();
-        clickedButton.setStyle("-fx-background-color: #8608a5");
+    private Button Boton11;
+
+    public void seleccionado() {
+        //boton.getid
+        Boton11.setStyle("-fx-background-color: green");
     }
+
 }
