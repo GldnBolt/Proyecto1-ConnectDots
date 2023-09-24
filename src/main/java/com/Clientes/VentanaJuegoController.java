a9f569b013118d51aad9bd6cfb58bd1bd9bb1f76
genecmmm <129640770+genecmmm@users.noreply.github.com>
2023-09-23 19:45:27 -0600

    Cuando los botones cambain de color tambien se hacen circulares

--- src/main/java/com/Clientes/VentanaJuegoController.java
+++ src/main/java/com/Clientes/VentanaJuegoController.java
@@ -11,9 +11,13 @@ import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class VentanaJuegoController{
+
+
     @FXML
     private void handleButtonAction(ActionEvent event) {
         Button clickedButton = (Button) event.getSource();
-        clickedButton.setStyle("-fx-background-color: #8608a5");
+        clickedButton.setStyle("-fx-background-color: #8608a5; -fx-background-radius: 50%; -fx-min-width: 50px; -fx-min-height: 50px; -fx-max-width: 50px; -fx-max-height: 50px;");
+
+
     }
 }
